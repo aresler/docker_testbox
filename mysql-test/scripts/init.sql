9a89e60cd66a5e7133d62914768e3e4f4d9b7593
andrew <andrey.resler@yandex.ru>
2023-07-19 17:23:29 +0200

    Added table relations to databases

--- mysql-test/scripts/init.sql
+++ mysql-test/scripts/init.sql
@@ -1,9 +1,20 @@
-CREATE TABLE IF NOT EXISTS foo (
+CREATE TABLE IF NOT EXISTS company (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    position VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL
 )  ENGINE=INNODB;
 
-INSERT INTO foo (name, position) VALUES ('bob', 'programmer');
-INSERT INTO foo (name, position) VALUES ('alice', 'manager');
-INSERT INTO foo (name, position) VALUES ('john', 'janitor');
+CREATE TABLE IF NOT EXISTS employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    position VARCHAR(255) NOT NULL,
+    company_id INT NOT NULL,
+    FOREIGN KEY (company_id) REFERENCES company(id) ON DELETE CASCADE
+)  ENGINE=INNODB;
+
+INSERT INTO company (name) VALUES ('Microsoft');
+INSERT INTO company (name) VALUES ('Oracle');
+INSERT INTO company (name) VALUES ('IBM');
+
+INSERT INTO employees (name, position, company_id) VALUES ('bob', 'programmer', 1);
+INSERT INTO employees (name, position, company_id) VALUES ('alice', 'manager', 2);
+INSERT INTO employees (name, position, company_id) VALUES ('john', 'janitor', 3);
--- postgres-test/scripts/init.sql
+++ postgres-test/scripts/init.sql
@@ -1,9 +1,20 @@
-CREATE TABLE foo(
+CREATE TABLE company(
     id serial PRIMARY KEY,
-    name VARCHAR (255) NOT NULL,
-    position VARCHAR (255) NOT NULL
+    name VARCHAR (255) NOT NULL
 );
 
-INSERT INTO foo (name, position) VALUES ('bob', 'programmer');
-INSERT INTO foo (name, position) VALUES ('alice', 'manager');
-INSERT INTO foo (name, position) VALUES ('john', 'janitor');
+CREATE TABLE employees(
+    id serial PRIMARY KEY,
+    name VARCHAR (255) NOT NULL,
+    position VARCHAR (255) NOT NULL,
+	company_id int NOT NULL,
+	FOREIGN KEY (company_id) REFERENCES company(id) ON DELETE CASCADE
+);
+
+INSERT INTO company (name) VALUES ('Microsoft');
+INSERT INTO company (name) VALUES ('Oracle');
+INSERT INTO company (name) VALUES ('IBM');
+
+INSERT INTO employees (name, position, company_id) VALUES ('bob', 'programmer', 1);
+INSERT INTO employees (name, position, company_id) VALUES ('alice', 'manager', 2);
+INSERT INTO employees (name, position, company_id) VALUES ('john', 'janitor', 3);
